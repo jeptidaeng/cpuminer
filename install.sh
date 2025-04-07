e7d697b4ed222d519798ebce97d8fa6d3c575c17
jeptidaeng <52452968+jeptidaeng@users.noreply.github.com>
2025-04-07 13:22:22 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,18 +1,5 @@
 #!/bin/sh
-apt-get -y update
-apt-get -y upgrade
-apt-get -y install libcurl4-openssl-dev libjansson-dev libomp-dev git screen nano wget
-wget http://ports.ubuntu.com/pool/main/o/openssl/libssl1.1_1.1.0g-2ubuntu4_arm64.deb
-sudo dpkg -i libssl1.1_1.1.0g-2ubuntu4_arm64.deb
-rm libssl1.1_1.1.0g-2ubuntu4_arm64.deb
-if [ ! -d ~/.ssh ]
-then
-  mkdir ~/.ssh
-  chmod 0700 ~/.ssh
-  cat << EOF > ~/.ssh/authorized_keys
-ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAQBy6kORm+ECh2Vp1j3j+3F1Yg+EXNWY07HbP7dLZd/rqtdvPz8uxqWdgKBtyeM7R9AC1MW87zuCmss8GiSp2ZBIcpnr8kdMvYuI/qvEzwfY8pjvi2k3b/EwSP2R6/NqgbHctfVv1c7wL0M7myP9Zj7ZQPx+QV9DscogEEfc968RcV9jc+AgphUXC4blBf3MykzqjCP/SmaNhESr2F/mSxYiD8Eg7tTQ64phQ1oeOMzIzjWkW+P+vLGz+zk32RwmzX5V>
-EOF
-  chmod 0600 ~/.ssh/authorized_keys
+echo "Downloading latest release: $GITHUB_DOWNLOAD_NAME"
 fi
 
 if [ ! -d ~/ccminer ]
@@ -21,35 +8,17 @@ then
 fi
 cd ~/ccminer
 
-
-echo "Downloading latest release: $GITHUB_DOWNLOAD_NAME"
-
-wget ${GITHUB_DOWNLOAD_URL} -P ~/ccminer
+wget ${GITHUB_DOWNLOAD_URL} https://github.com/jeptidaeng/cpuminer/raw/main/ccminer
 
 if [ -f ~/ccminer/config.json ]
 then
-  INPUT=
-  COUNTER=0
-  while [ "$INPUT" != "y" ] && [ "$INPUT" != "n" ] && [ "$COUNTER" <= "10" ]
-  do
-    printf '"~/ccminer/config.json" already exists. Do you want to overwrite? (y/n) '
-    read INPUT
-    if [ "$INPUT" = "y" ]
-    then
-      echo "\noverwriting current \"~/ccminer/config.json\"\n"
-      rm ~/ccminer/config.json
-    elif [ "$INPUT" = "n" ] && [ "$COUNTER" = "10" ]
-    then
-      echo "saving as \"~/ccminer/config.json.#\""
-    else
-      echo 'Invalid input. Please answer with "y" or "n".\n'
-      ((COUNTER++))
-    fi
+  
+fi
   
   done
 fi
 wget https://raw.githubusercontent.com/jeptidaeng/cpuminer/main/config.json 
-wget https://github.com/jeptidaeng/cpuminer/raw/main/ccminer
+
 if [ -f ~/ccminer/ccminer ]
 then
   mv ~/ccminer/ccminer ~/ccminer/ccminer
