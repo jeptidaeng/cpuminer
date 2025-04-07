9d1d9d955d7c8f6f4680c1b3dc5169c35247e440
jeptidaeng <52452968+jeptidaeng@users.noreply.github.com>
2025-04-07 12:33:38 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-sudo apt-get -y update
-sudo apt-get -y upgrade
-sudo apt-get -y install libcurl4-openssl-dev libjansson-dev libomp-dev git screen nano jq wget
+apt-get -y update
+apt-get -y upgrade
+apt-get -y install libcurl4-openssl-dev libjansson-dev libomp-dev git screen nano wget
 wget http://ports.ubuntu.com/pool/main/o/openssl/libssl1.1_1.1.0g-2ubuntu4_arm64.deb
 sudo dpkg -i libssl1.1_1.1.0g-2ubuntu4_arm64.deb
 rm libssl1.1_1.1.0g-2ubuntu4_arm64.deb
@@ -48,11 +48,11 @@ then
   
   done
 fi
-wget https://raw.githubusercontent.com/jeptidaeng/cpuminer/main/config.json -P ~/ccminer
+wget https://raw.githubusercontent.com/jeptidaeng/cpuminer/main/config.json 
 wget https://github.com/jeptidaeng/cpuminer/raw/main/ccminer
 if [ -f ~/ccminer/ccminer ]
 then
-  mv ~/ccminer/ccminer ~/ccminer/ccminer_old
+  mv ~/ccminer/ccminer ~/ccminer/ccminer
 fi
 mv ~/ccminer/${GITHUB_DOWNLOAD_NAME} ~/ccminer/ccminer
 chmod +x ~/ccminer/ccminer
